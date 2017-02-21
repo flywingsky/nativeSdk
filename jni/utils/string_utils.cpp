2c186f9064cc04c56b44e5d09be55b7211287bd5
genglei-cuan <genglei.cuan@godinsec.com>
2017-02-21 21:40:58 +0800

    添加utils中toString方法

--- jni/utils/string_utils.cpp
+++ jni/utils/string_utils.cpp
@@ -1,16 +1,26 @@
+/**
+ * Project : weimi-native-sdk
+ * FileName: string_utils.cpp
+ * date    : 2017-02-20
+ * time    : 13:14:47
+ * author  : genglei.cuan@godinsec.com
+ */
+
 #include "string_utils.h"
-#include <cctype>
+#include <cctype> //isspace
 #include <string>
 #include <algorithm> // find_if
 
+
+
 std::string &godin::StringUtils::ltrim(std::string &s) {
-  std::iterator it = std::find_if_not(s.begin(), s.end(), [](int c){return isspace(c);});
+  std::string::iterator it = std::find_if_not(s.begin(), s.end(), [](int c){return isspace(c);});
   s.erase(s.begin(),it);
   return s;
 }
 
 std::string &godin::StringUtils::rtrim(std::string &s) {
-  std::iterator it = std::find_if_not(s.rbegin(), s.rend(), [](int c){return isspace(c);}).base();
+  std::string::iterator it = std::find_if_not(s.rbegin(), s.rend(), [](int c){return isspace(c);}).base();
   s.erase(it,s.end());
   return s;
 }
--- jni/utils/string_utils.h
+++ jni/utils/string_utils.h
@@ -3,6 +3,16 @@
 
 #include <base/macros.h>
 #include <string>
+#include <sstream>
+
+/**
+ * Project : weimi-native-sdk
+ * FileName: string_utils.h
+ * date    : 2017-02-20
+ * time    : 13:10:02
+ * author  : genglei.cuan@godinsec.com
+ */
+
 
 NS_GODIN_BEGIN
 
@@ -11,10 +21,13 @@ class StringUtils {
   private:
     StringUtils(){}
 
+  public:
   /**
    * @brief ltrim
    * 清除字符串起始位置处的所有空格
    * @param s
+   * 要被处理的字符串
+   * 由于是引用传递,所以处理后,原始字符串也发生了改变.
    * @return
    */
   static std::string& ltrim(std::string& s);
@@ -23,6 +36,8 @@ class StringUtils {
    * @brief rtrim
    * 清除字符串结束位置处的所有空格
    * @param s
+   * 要被处理的字符串
+   * 由于是引用传递,所以处理后,原始字符串也发生了改变.
    * @return
    */
   static std::string& rtrim(std::string& s);
@@ -31,9 +46,18 @@ class StringUtils {
    * @brief trim
    * 清除字符串起始和结束位置处的所有空格
    * @param s
+   * 要被处理的字符串
+   * 由于是引用传递,所以处理后,原始字符串也发生了改变.
    * @return
    */
   static std::string& trim(std::string& s);
+
+  template<typename T>
+  static std::string toString(T value){
+    std::ostringstream os;
+    os << value;
+    return os.str();
+  }
 };
 
 NS_GODIN_END
