20fa99adeb665271cbec0acebf33e9956312dc9a
genglei-cuan <genglei.cuan@godinsec.com>
2017-02-23 17:38:32 +0800

    添加Hashutils类用于计算常见hash值

---
+++ jni/utils/hash_utils.cpp
@@ -0,0 +1,90 @@
+#include "hash_utils.h"
+#include <cryptopp/hex.h>
+#include <cryptopp/files.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/crc.h>
+#include "file_utils.h"
+
+bool godin::HashUtils::getFileSha1HexStr(const std::string &path, std::string &shavale) {
+
+  if(!godin::FileUtils::isFileExists(path))
+    return false;
+
+  CryptoPP::SHA1 sha1;
+  CryptoPP::FileSource(path.c_str(),true,
+                       new CryptoPP::HashFilter(sha1,
+                          new CryptoPP::HexEncoder(
+                          new CryptoPP::StringSink(shavale)))
+                       );
+  return true;
+}
+
+bool godin::HashUtils::getFileSha1(const std::string &path, uint8_t *buf, int len) {
+  if(buf == NULL || len == 0)
+    return false;
+
+  if(!godin::FileUtils::isFileExists(path))
+    return false;
+
+  CryptoPP::SHA1 sha1;
+  CryptoPP::FileSource(path.c_str(),true,
+                       new CryptoPP::HashFilter(sha1,
+                          new CryptoPP::ArraySink(buf,len))
+                       );
+  return true;
+}
+
+bool godin::HashUtils::getBlockSha1HexStr(uint8_t *data, size_t size, std::string &shavale) {
+
+  if(data == NULL || size == 0)
+    return false;
+
+  CryptoPP::SHA1 sha1;
+  CryptoPP::StringSource(data,size,true,
+                         new CryptoPP::HashFilter(sha1,
+                            new CryptoPP::HexEncoder(
+                            new CryptoPP::StringSink(shavale))));
+  return true;
+}
+
+bool godin::HashUtils::getBlockSha1(uint8_t *data, size_t size, uint8_t *buf, int len) {
+
+  if(data == NULL || size == 0)
+    return false;
+  if(data == buf || len == 0)
+    return false;
+
+  CryptoPP::SHA1 sha1;
+  CryptoPP::StringSource(data,size,true,
+                         new CryptoPP::HashFilter(sha1,
+                            new CryptoPP::ArraySink(buf,len)));
+  return true;
+}
+
+bool godin::HashUtils::getBlockCrc32HexStr(uint8_t *data, size_t size, std::string &crcvalue) {
+
+  if(data == NULL || size == 0)
+    return false;
+
+  CryptoPP::CRC32 crc;
+  CryptoPP::StringSource(data,size,true,
+                         new CryptoPP::HashFilter(crc,
+                            new CryptoPP::HexEncoder(
+                            new CryptoPP::StringSink(crcvalue))));
+  return true;
+}
+
+bool godin::HashUtils::getBlockCrc32(uint8_t *data, size_t size, uint8_t *buf, int len) {
+
+  CryptoPP::CRC32 crc;
+
+  if(data == NULL || size == 0)
+    return false;
+  if(data == buf || len < crc.DigestSize())
+    return false;
+
+  crc.Update(data,size);
+  crc.TruncatedFinal(buf,len);
+
+  return true;
+}
---
+++ jni/utils/hash_utils.h
@@ -0,0 +1,37 @@
+#ifndef HASHUTILS_H
+#define HASHUTILS_H
+
+#include <base/macros.h>
+#include <string>
+
+NS_GODIN_BEGIN
+
+/**
+ * @brief The HashUtils class
+ *
+ * 使用 cryptopp 计算 hash
+ */
+
+class HashUtils {
+
+  public:
+
+    static bool getFileSha1HexStr(const std::string &path,std::string &shavale);
+
+    static bool getFileSha1(const std::string &path,uint8_t *buf,int len);
+
+    static bool getBlockSha1HexStr(uint8_t* data,size_t size,std::string &shavale);
+
+    static bool getBlockSha1(uint8_t* data,size_t size,uint8_t* buf,int len);
+
+    static bool getBlockCrc32HexStr(uint8_t* data,size_t size,std::string &crcvalue);
+
+    static bool getBlockCrc32(uint8_t* data,size_t size,uint8_t* buf,int len);
+
+  private:
+    HashUtils(){}
+};
+
+NS_GODIN_END
+
+#endif // HASHUTILS_H
