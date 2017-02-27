d05d531921f400b023203e2c5a179e3d069a7f02
genglei-cuan <genglei.cuan@godinsec.com>
2017-02-27 21:01:35 +0800

    添加file类的代码实现

--- jni/file/file.cpp
+++ jni/file/file.cpp
@@ -6,5 +6,150 @@
  * author  : genglei.cuan@godinsec.com
  */
 #include "file.h"
+#include <libgen.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <utils/log.h>
+#include <utils/file_utils.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 
+
+godin::File::File(const std::string &name) {
+  path_name.clear();
+  file_name.clear();
+  data = NULL;
+  open_mode = godin::File::NotOpen;
+  read_only = false;
+  fd = -1;
+
+  char *temp_path = realpath(name.c_str(),NULL);
+
+  if(temp_path == NULL){
+   godin::Log::e(" file path :%s ,is invaild. please check it berfore.",name.c_str());
+  }else{
+   path_name = temp_path;
+   free(temp_path);
+   temp_path = NULL;
+   file_name = basename((char *)path_name.c_str());
+  }
+}
+
+bool godin::File::exists() const {
+  if(path_name.empty())
+    return false;
+  return godin::FileUtils::isFileExists(path_name);
+}
+
+std::string godin::File::suffix() const {
+  return godin::FileUtils::getSuffix(file_name);
+}
+
+size_t godin::File::fileSize() const {
+  return godin::FileUtils::getFileSize(path_name);
+}
+
+std::string godin::File::fileDir() const {
+
+  if(path_name.find_last_of("/") != std::string::npos){
+      return path_name.substr(0,path_name.find_last_of("/")+1);
+  }else{
+      return "";
+  }
+}
+
+bool godin::File::remove() {
+
+  if(isOpen())
+    close();
+
+  return godin::FileUtils::removeFile(path_name);
+}
+
+bool godin::File::rename(const std::string &newName) {
+
+
+  std::string  current_file_dir_path = fileDir();
+
+  if(current_file_dir_path.empty())
+    return false;
+
+  if(isOpen())
+    close();
+
+  std::string suffix = (this->suffix().empty() == false) ? (std::string(".")+this->suffix()) : "";
+  std::string new_name = current_file_dir_path + newName + suffix;
+  if(::rename(path_name.c_str(),new_name.c_str()) == 0){
+      path_name = new_name;
+      file_name = newName + suffix;
+      return true;
+  }else{
+      return false;
+    }
+}
+
+bool godin::File::resize(size_t newSize) {
+
+  if(isOpen())
+    close();
+
+
+}
+
+bool godin::File::isOpen() const {
+  return fd > 0 ? true : false;
+}
+
+bool godin::File::isReadable() const {
+
+  return fd >0 ? true : false;
+
+}
+
+bool godin::File::isWritable() const {
+  if(fd>0 && open_mode != ReadOnly)
+    return true;
+  else {
+      return false;
+  }
+}
+
+bool godin::File::open(godin::File::OpenMode mode) {
+
+  switch (mode) {
+    case godin::File::ReadOnly:
+      fd = ::open(path_name.c_str(),O_RDONLY);
+      open_mode = ReadOnly;
+      break;
+    case godin::File::ReadWrite:
+      fd = ::open(path_name.c_str(),O_RDWR);
+      ReadOnly = ReadWrite;
+      break;
+    case godin::File::Append:
+      fd = ::open(path_name.c_str(),O_RDWR | O_APPEND);
+      ReadOnly = Append;
+      break;
+    case godin::File::Truncate:
+      fd = ::open(path_name.c_str(),O_RDWR | O_TRUNC);
+      ReadOnly = Truncate;
+      break;
+    default:
+      fd = -1;
+      ReadOnly = NotOpen;
+      godin::Log::e("open file mode is not support. please check it before.");
+      break;
+    }
+  if(fd > 0)
+    return true;
+  else
+    return false;
+}
+
+void godin::File::close() {
+  if(fd > 0){
+    ::close(fd);
+  }
+}
--- jni/file/file.h
+++ jni/file/file.h
@@ -15,23 +15,23 @@ NS_GODIN_BEGIN
 
 class File {
   public:
-    File();
     File(const std::string &name);
 
   public:
     enum OpenMode {
-          NotOpen = 0x0000,
-          ReadOnly = 0x0001,
-          WriteOnly = 0x0002,
-          ReadWrite = ReadOnly | WriteOnly,
-          Append = 0x0004,
-          Truncate = 0x0008,
-          Text = 0x0010,
+          NotOpen    = 0x0000, ///< 还没打开
+          ReadOnly   = 0x0001, ///< 以只读方式打开
+          ReadWrite  = 0x0002, ///< 以可读可写方式打开
+          Append     = 0x0003, ///< 以可读可写追加方式
+          Truncate   = 0x0004, ///< 以可读可写方式打开,并清空文件内容
+          NoSupport  = 0x0005,
       };
     /**
      * @brief exists
      * 检查文件是否存在
      * @return
+     * false: 不存在,或者path_name 为空
+     * true: 存在
      */
     bool exists() const;
 
@@ -47,14 +47,14 @@ class File {
      * 获取文件名(不包括路径)
      * @return
      */
-    std::string fileName() const;
+    std::string fileName() const { return file_name;}
 
     /**
      * @brief pathName
-     * 获取文件的全路径名(path + fileName)
+     * 获取文件的绝对路径名(path + fileName)
      * @return
      */
-    std::string pathName() const;
+    std::string pathName() const { return path_name;}
 
     /**
      * @brief fileSize
@@ -62,6 +62,13 @@ class File {
      * @return
      */
     size_t fileSize() const;
+
+    /**
+     * @brief fileDir
+     * 返回当前文件所在的文件夹绝对路径(结尾包括"/")
+     * @return
+     */
+    std::string fileDir()  const;
 
     /**
      * @brief remove
@@ -76,10 +83,14 @@ class File {
      * 文件重命名；
      * 如果newName文件已经存在，则返回false,即不重命名不会覆盖已有文件.
      * 重命名前，会先关闭文件
+     *
      * @param newName
+     * 特别要注意的是,传入的是文件的新名字,不包括路径和后缀;
+     * 成功改名后,文件会被关闭,内部path_name和file_name均会更新.
+     * 另外该方法不能修改文件的后缀名.
      * @return
      */
-    bool rename(const std::string newName);
+    bool rename(const std::string &newName);
 
     /**
      * @brief resize
@@ -89,14 +100,70 @@ class File {
      */
     bool resize(size_t newSize);
 
+    /**
+     * @brief isOpen
+     * 文件是否已经打开
+     * @return
+     */
     bool isOpen() const;
+
+    /**
+     * @brief isReadable
+     * 文件已经打开,并且可读
+     * @return
+     */
     bool isReadable() const;
+
+    /**
+     * @brief isWritable
+     * 文件已经打开,并且可写
+     * @return
+     */
     bool isWritable() const;
+
+    /**
+     * @brief open
+     * 打开文件
+     * @param mode
+     * @return
+     */
     bool open(OpenMode mode);
+
+    /**
+     * @brief close
+     * 关闭文件,并且释放资源
+     */
     void close();
 
+    /**
+     * @brief fd
+     * 返回fd.
+     * @return
+     */
+    int fd() const {return fd;}
 
-};
+    /**
+     * @brief openMode
+     * 返回文件的打开模式
+     * @return
+     */
+    OpenMode openMode() const { return open_mode;}
+
+    private:
+
+      DISALLOW_COPY_AND_ASSIGN(File);
+      int fd; ///< 文件句柄
+
+      std::string path_name; ///<  路径 + 文件名
+
+      std::string file_name; ///< 文件名
+
+      char *data; ///< 文件数据指针
+
+      bool read_only; ///< flag. 只读标志
+
+      OpenMode open_mode;
+    };
 
 
 NS_GODIN_END
