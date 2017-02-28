cc4bcff3d5eefeeb331172914df750c2aa088dfc
genglei.cuan <genglei.cuan@godinsec.com>
2017-03-01 01:59:36 +0800

    添加time类

--- jni/date/time.cpp
+++ jni/date/time.cpp
@@ -1,6 +1,20 @@
+/**
+ * Project : weimi-native-sdk
+ * FileName: time.cpp
+ * date    : 2017-03-01
+ * time    : 00:35:40
+ * author  : genglei.cuan@godinsec.com
+ */
 #include "time.h"
+#include <sys/time.h>
+#include <time.h>
 
-Time::Time()
-{
+int64_t godin::Time::currentTimeMillis() {
+  timeval now;
 
+  ::gettimeofday(&now, NULL);
+
+  int64_t when = now.tv_sec * 1000LL + now.tv_usec / 1000;
+
+  return when;
 }
--- jni/date/time.h
+++ jni/date/time.h
@@ -1,11 +1,32 @@
+/**
+ * Project : weimi-native-sdk
+ * FileName: time.h
+ * date    : 2017-03-01
+ * time    : 00:35:31
+ * author  : genglei.cuan@godinsec.com
+ */
 #ifndef TIME_H
 #define TIME_H
 
+#include <base/macros.h>
+#include <stdio.h>
 
-class Time
-{
-public:
-  Time();
+NS_GODIN_BEGIN
+
+class Time {
+  private:
+    Time();
+  public:
+
+    /**
+     * @brief currentTimeMillis
+     * 返回系统当前的时间，毫秒计算；
+     * 可用来计算某代码块运行时间时使用
+     * @return
+     */
+    static int64_t currentTimeMillis();
 };
+
+NS_GODIN_END
 
 #endif // TIME_H
